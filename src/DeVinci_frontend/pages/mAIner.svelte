a5c778577bd30c5a99c2add1d039d3a253275af1
2n1u0 <388476+lopezi@users.noreply.github.com>
2025-02-15 18:44:36 +0000

    style to mainer page

--- src/DeVinci_frontend/pages/mAIner.svelte
+++ src/DeVinci_frontend/pages/mAIner.svelte
@@ -2,12 +2,12 @@
   import { store } from "../store";
 </script>
 
-<div class="container mx-auto p-8">
-  <h1 class="text-2xl font-bold mb-6">mAIner</h1>
+<div class="container mx-auto h-lvh p-8 bg-[radial-gradient(circle_at_center,_#300030,_#170017,_#0c000c)]">
+  <h1 class="text-2xl font-bold mb-6 text-gray-100">mAIner</h1>
   
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
     <!-- Add your dashboard content here -->
-    <div class="bg-gray-100 p-6 rounded-lg shadow">
+    <div class="bg-gray-100/50  p-6 rounded-lg shadow">
       <h2 class="text-sm font-semibold mb-4">Activity feed</h2>
       <div class="card-style p-4 rounded-lg shadow mb-2">
         <ul>
@@ -18,7 +18,7 @@
       </div>
     </div>
 
-    <div class="bg-gray-100 p-6 rounded-lg shadow">
+    <div class="bg-gray-100/50 p-6 rounded-lg shadow">
       <h2 class="text-sm font-semibold mb-4">Flags</h2>
       <div class="card-style p-4 rounded-lg shadow mb-2">
         <ul>
@@ -29,7 +29,7 @@
       </div>
     </div>
 
-    <div class="bg-gray-100 p-6 rounded-lg shadow">
+    <div class="bg-gray-100/50 p-6 rounded-lg shadow">
       <h2 class="text-sm font-semibold mb-4">Stats</h2>
       <div class="card-style p-4 rounded-lg shadow mb-2">
         <ul>
@@ -40,7 +40,7 @@
       </div>
     </div>
 
-    <div class="bg-gray-100 p-6 rounded-lg shadow">
+    <div class="bg-gray-100/50 p-6 rounded-lg shadow">
       <h2 class="text-sm font-semibold mb-4">Top up cycles</h2>
       <div class="card-style p-4 rounded-lg shadow mb-2">
         <ul>
@@ -50,15 +50,5 @@
         </ul>
       </div>
     </div>
-
   </div>
 </div> 
-
-
-
-Last submitted responses
-Any flags that indicate needed attention
-Too low cycles
-Stats
-Cycle balance and burn rate
-Top up cycles (potentially as function on Game State due to payment)
