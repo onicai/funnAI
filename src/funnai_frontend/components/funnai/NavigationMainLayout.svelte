ef6f77ccf70931c5c60ab232cb27c0ddfdae4ee8
Nuno Lopes <n@lopezi.com>
2025-06-17 19:15:48 +0100

    fix icon

--- src/funnai_frontend/components/funnai/NavigationMainLayout.svelte
+++ src/funnai_frontend/components/funnai/NavigationMainLayout.svelte
@@ -119,7 +119,7 @@
       </div> -->
       <a use:link href="/wallet" class="flex-shrink-0 text-gray-900 dark:text-orange-400 bg-orange-100 dark:bg-gray-700 border-2 border-orange-500 hover:border-2 hover:border-orange-600 dark:hover:border-orange-400 hover:bg-orange-200 dark:hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-orange-200 dark:focus:ring-orange-700 font-medium rounded-lg text-xs sm:text-sm px-2 sm:px-5 py-2 sm:py-2.5 text-center inline-flex items-center transition-colors min-h-[44px]" style="box-shadow: rgb(214, 195, 219) 0px 0px 2px 0px; border-radius: 16px;">
         <svg class="w-4 h-4 me-1 sm:me-2 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3 3v8a3 3 0 003 3z"></path>
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"></path>
         </svg>
         <span class="hidden xs:inline">Wallet</span>
       </a>
