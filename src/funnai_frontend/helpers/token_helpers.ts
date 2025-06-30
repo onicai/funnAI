16316f5abfdc4d05fa2015f4493ca479fd0bbd40
patnorris <patrick@DESKTOP-TQ4V8LQ.localdomain>
2025-06-30 13:25:12 +0200

    Add demo token for testing on demo stage

--- src/funnai_frontend/helpers/token_helpers.ts
+++ src/funnai_frontend/helpers/token_helpers.ts
@@ -118,6 +118,29 @@ const availableTokens: FE.Token[] = [ // TODO
     token_id: 4,
     chain: 'IC',
     total_24h_volume: '0'
+  },
+  {
+    canister_id: "z6s3y-4aaaa-aaaaj-a2bjq-cai",
+    name: "FUNNAIdemo",
+    symbol: "FUNNAIdemo",
+    decimals: 8,
+    address: "z6s3y-4aaaa-aaaaj-a2bjq-cai",
+    fee: 0.0000001,
+    fee_fixed: "10",
+    token: "z6s3y-4aaaa-aaaaj-a2bjq-cai",
+    icrc1: true,
+    icrc2: true,
+    icrc3: true,
+    pool_symbol: "",
+    pools: [],
+    timestamp: Date.now(),
+    metrics: null,
+    balance: '0',
+    logo_url: "/coin.webp",
+    token_type: 'IC',
+    token_id: 4,
+    chain: 'IC',
+    total_24h_volume: '0'
   }
 ];
 
