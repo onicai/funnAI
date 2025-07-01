eddf39bfdb12da802dba21fd0072cece44ac2448
icpp <icpp@icpp.world>
2025-06-30 23:52:35 -0400

    Script to clean the prompt cache files in the LLMs

---
+++ scripts/cleanup_llm_promptcache.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+import time
+import argparse
+import os
+from collections import defaultdict
+from dotenv import dotenv_values
+import json
+
+from .monitor_common import get_canisters, ensure_log_dir
+from datetime import datetime, timezone
+
+# Get the directory of this script
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def cleanup_llm_promptcache(gamestate_canister_id, judge_canister_id, share_service_canister_id, canister_name, canister_id, network):
+    """Cleanup prompt cache files in the LLM."""
+
+    llm_type = ""
+    if "CHALLENGER" in canister_name.upper():
+        llm_type = "challenger"
+    elif "JUDGE" in canister_name.upper():
+        llm_type = "judge"
+    elif "SHARE_SERVICE" in canister_name.upper():
+        llm_type = "share_service"
+    else:
+        print(f"Unknown llm type for canister {canister_name}. Skipping cleanup.")
+        return
+
+    try:
+        # -------------------------------------------------------------
+        # We can delete the prompt cache files of all archived & closed challenges
+        # -------------------------------------------------------------
+
+        judgePromptIds = []
+        mainerPromptIds = []
+
+        if network != "prd":
+            # Ok for testing on demo network, where we do not have any archived challenges
+            print(f"Cleaning up prompt cache for closed challenges on network {network}...")
+            # Get promptIds for the closed challenges
+            result = subprocess.check_output(
+                ["dfx", "canister", "call", gamestate_canister_id, "getClosedChallengesAdmin", "--output", "json", "--network", network],
+                stderr=subprocess.DEVNULL,
+                text=True
+            )
+            data = json.loads(result)
+            for challenge in data.get('Ok', []):
+                if challenge.get('judgePromptId'):
+                    judgePromptIds.append(challenge['judgePromptId'])
+                if challenge.get('mainerPromptId'):
+                    mainerPromptIds.append(challenge['mainerPromptId'])
+        else:
+            print(f"Skipping cleanup of closed challenges on production network {network} for canister {canister_name} ({canister_id}).")
+            print("We only close prompt caches for archived challenges on production network.")
+
+        # get promptIds for archived challenges
+        print(f"Cleaning up prompt cache for archived challenges on network {network}...")
+        result = subprocess.check_output(
+            ["dfx", "canister", "call", gamestate_canister_id, "getArchivedChallengesAdmin", "--output", "json", "--network", network],
+            stderr=subprocess.DEVNULL,
+            text=True
+        )
+        data = json.loads(result)
+        for challenge in data.get('Ok', []):
+            if challenge.get('judgePromptId'):
+                judgePromptIds.append(challenge['judgePromptId'])
+            if challenge.get('mainerPromptId'):
+                mainerPromptIds.append(challenge['mainerPromptId'])
+
+        if not judgePromptIds and not mainerPromptIds:
+            print(f"No closed or archived promptIds found for {llm_type} canister {canister_name} ({canister_id}) on network {network}. Nothing to clean up.")
+            return
+        
+        # NOTE: we do not have a clean remove_promptcache, because only the Judge Controller can remove it
+        #       a patch is to upload a zero byte file to the prompt cache, which will overwrite the existing cache file
+        if llm_type == "judge" or llm_type == "all":
+            # sample Judge: .canister_cache/qmgdh-3aaaa-aaaaa-qanfq-cai/sessions/bc0e24ac-7ef4-4d19-a061-ec5324a9ca74.cache
+            for judgePromptId in judgePromptIds:
+                full_prompt_cache_filename = f".canister_cache/{judge_canister_id}/sessions/{judgePromptId}.cache"
+                subprocess.run(
+                    ["dfx", "canister", "call", canister_id, "file_upload_chunk", 
+                    f"(record {{filename = \"{full_prompt_cache_filename}\"; chunk = blob \"\"; chunksize = 0 : nat64; offset = 0 : nat64; }})", 
+                    "--network", network],
+                    check=True,
+                    text=True
+                )
+                # delayed to avoid backups
+                time.sleep(3)
+
+        if llm_type == "share_service" or llm_type == "all":
+            # sample ShareService: .canister_cache/rilmv-caaaa-aaaaa-qandq-cai/sessions/d85d3b4f-4e91-4102-a8bb-96b5a50ec398.cache
+            for mainerPromptId in mainerPromptIds:
+                full_prompt_cache_filename = f".canister_cache/{share_service_canister_id}/sessions/{mainerPromptId}.cache"
+                subprocess.run(
+                    ["dfx", "canister", "call", canister_id, "file_upload_chunk", 
+                    f"(record {{filename = \"{full_prompt_cache_filename}\"; chunk = blob \"\"; chunksize = 0 : nat64; offset = 0 : nat64; }})", 
+                    "--network", network],
+                    check=True,
+                    text=True
+                )
+                # delayed to avoidbackups
+                time.sleep(3)
+    except subprocess.CalledProcessError:
+        return []
+
+def main(network, llm_type):
+    (CANISTERS, CANISTER_COLORS, RESET_COLOR) = get_canisters(network, "protocol")
+
+    gamestate_name = None
+    gamestate_canister_id = None
+    judge_name = None
+    judge_canister_id = None
+    share_service_name = None
+    share_service_canister_id = None
+    for name, canister_id in CANISTERS.items():
+        if "LLM" in name.upper():
+            continue  # Skip LLM canisters in this loop
+        if "JUDGE" in name.upper():
+            judge_name = name
+            judge_canister_id = canister_id
+        elif "SERVICE" in name.upper():
+            share_service_name = name
+            share_service_canister_id = canister_id
+        elif "GAMESTATE" in name.upper():
+            gamestate_name = name
+            gamestate_canister_id = canister_id
+        
+        if gamestate_name and judge_name and share_service_name:
+            # We found all necessary canisters, no need to continue
+            print(f"Found canisters: GAMESTATE={gamestate_name}, JUDGE={judge_name}, SHARE_SERVICE={share_service_name}")   
+            break
+            
+    if not gamestate_canister_id:
+        print(f"No GAMESTATE canister found in canisters-{network}.env")
+        return
+    if not judge_canister_id:
+        print(f"No JUDGE canister found in canisters-{network}.env")
+        return
+    if not share_service_canister_id:
+        print(f"No SHARE_SERVICE canister found in canisters-{network}.env")
+        return
+
+    for name, canister_id in CANISTERS.items():
+        if "LLM" in name.upper() and not "CHALLENGER" in name.upper() and (llm_type == 'all' or llm_type.lower() in name.lower()):
+            print(f"Cleaning up LLM prompt cache for {name} ({canister_id}) on network {network}...")
+            cleanup_llm_promptcache(gamestate_canister_id, judge_canister_id, share_service_canister_id, name, canister_id, network)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Monitor DFINITY canister logs.")
+    parser.add_argument(
+        "--network",
+        choices=["local", "ic", "testing", "demo", "development", "prd"],
+        default="local",
+        help="Specify the network to use (default: local)",
+    )
+    parser.add_argument(
+        "--llm-type",
+        choices=["all", "judge", "share_service"],
+        default="all",
+        help="Specify the cache type to use (default: all)",
+    )
+    args = parser.parse_args()
+    main(args.network, args.llm_type)
---
+++ scripts/cleanup_llm_promptcache.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#######################################################################
+# run from parent folder as:
+# scripts/log.sh --network [local|ic]
+#######################################################################
+
+# Default network type is local
+NETWORK_TYPE="local"
+LLM_TYPE="all"
+
+# Parse command line arguments for network type
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --network)
+            shift
+            if [ "$1" = "local" ] || [ "$1" = "ic" ] || [ "$1" = "testing" ] || [ "$1" = "development" ] || [ "$1" = "demo" ] || [ "$1" = "prd" ]; then
+                NETWORK_TYPE=$1
+            else
+                echo "Invalid network type: $1. Use 'local' or 'ic' or 'testing' or 'development' or 'demo' or 'prd'."
+                exit 1
+            fi
+            shift
+            ;;
+        --llm-type)
+            shift
+            if [ "$1" = "all" ] || [ "$1" = "judge" ] || [ "$1" = "share_service" ]; then
+                LLM_TYPE=$1
+            else
+                echo "Invalid --llm-type: $1. Use 'all' or 'challenger' or 'judge' or 'share_service'."
+                exit 1
+            fi
+            shift
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            echo "Usage: $0 --network [local|ic|testing|development|demo|prd] [--llm-type [all|judge|share_service]]"
+            exit 1
+            ;;
+    esac
+done
+
+echo "Using network type: $NETWORK_TYPE"
+echo "Using cache type: $LLM_TYPE"
+
+python -m scripts.cleanup_llm_promptcache --network $NETWORK_TYPE --llm-type $LLM_TYPE
