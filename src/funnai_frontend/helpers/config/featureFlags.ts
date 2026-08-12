2b456c81bd64ea0e801c5c1662f32e559170edd0
Nuno Lopes <n@lopezi.com>
2026-08-12 15:52:35 +0100

    (fix): enables marketplace

--- src/funnai_frontend/helpers/config/featureFlags.ts
+++ src/funnai_frontend/helpers/config/featureFlags.ts
@@ -3,7 +3,7 @@
  * Flip these to enable/disable product surfaces without a canister upgrade.
  * Re-enable marketplace by setting MARKETPLACE_ENABLED to true after the issue is resolved.
  */
-export const MARKETPLACE_ENABLED = false;
+export const MARKETPLACE_ENABLED = true;
 
 export const MARKETPLACE_DISABLED_MESSAGE =
   'The marketplace is temporarily unavailable and will return soon.';
